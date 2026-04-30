50a0f6d71e14d19850019e54418a976618ba6e33
jotinhadevs <sousa.pedro2@acad.ifma.edu.br>
2026-04-30 09:36:32 -0300

    Adicionando area do losango

--- calc_areas.h
+++ calc_areas.h
@@ -2,3 +2,4 @@ float calcAreaQuadrado(float lado);
 float calcAreaTriangulo(float base, float altura);
 float calcAreaRetangulo(float base, float altura);
 float calcAreaTrianguloIsosceles(float base, float altura);
+float calcAreaLosango(float diagonal_maior , float diagonal_menor);
