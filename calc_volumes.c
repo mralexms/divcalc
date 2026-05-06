5f823f68971871c3614e834f2057fd0dd68b14b9
José Isac <isac.j@acad.ifma.edu.br>
2026-05-06 17:11:02 -0300

    adicionando o volume da esfera

--- calc_volumes.c
+++ calc_volumes.c
@@ -11,3 +11,7 @@ float calcVolumeCilindro(float raio,float altura){
 	3.14 * raio * raio * altura;
 } //Dayseanne
 >>>>>>> 77944600b110c63efc226bda2c70ff980a54ce52
+
+float CalcVolEsfera(float raio){
+  (4/3*3.14)*raio*raio*raio;
+}
