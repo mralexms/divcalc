fc0ba8125b8303d5cc937a2ee8030c7adda45d6a
José Isac <isac.j@acad.ifma.edu.br>
2026-05-06 17:16:00 -0300

    Agora o .h

--- calc_volumes.h
+++ calc_volumes.h
@@ -4,3 +4,5 @@ float calcVolumePiramide(float areaBase,float altura);
 =======
 float calcVolumeCilindro(float raio,float altura); //Dayseanne
 >>>>>>> 77944600b110c63efc226bda2c70ff980a54ce52
+
+float CalcVolEsfera(float raio);
