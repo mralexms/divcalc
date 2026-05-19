7a294b6057a06bd8367b9655e8973cfc3b0de9e1
dudasmontel-ops <dudas.montel@gmail.com>
2026-05-19 11:45:35 -0300

    adicionada a área da base, lateral e total do cone.

--- calc_areas.c
+++ calc_areas.c
@@ -30,3 +30,16 @@ float calcAreaCirculo(float raio){
 float calcAreaEsfera(float raio){
 	return 4 * 3.14 * (raio * raio);
 }
+//Duda
+float calcAreaBaseCone(float raio){
+    return 3.14*(raio*raio);
+}
+float calcGeratriz(float raio, float altura){
+    return sqrt((raio * raio) + (altura * altura));
+}
+float calcAreaLateralCone(float raio, float geratriz){
+    return 3.14 * raio * geratriz;
+}
+float calcAreaTotalCone(float areaBase, float areaLateral){
+    return areaBase + areaLateral;
+}
