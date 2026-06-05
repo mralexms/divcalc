b03a27180a812f728f8f1a3e823b7899e90c0ae7
arthuralmeida13 <a.arthur@acad.ifma.edu.br>
2026-06-05 15:00:24 -0300

    Update calc_volumes.h

--- calc_volumes.h
+++ calc_volumes.h
@@ -4,4 +4,3 @@ float calcVolumeCilindro(float raio,float altura); //Dayseanne
 float CalcVolEsfera(float raio);
 float calcVolumeCone(float raio, float altura);
 float calcVolumePrisma(float areaBase, float altura);
-float calcVolumeParalelepipedo(float largura, float altura, float comprimento);
